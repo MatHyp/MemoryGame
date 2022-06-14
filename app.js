d13e4171614cc8a0e61bc8009d0e79eefc324079
MatHyp <matijoker29@gmail.com>
2022-06-14 20:43:29 +0200

    add hidden class after some period of time

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
-const startBtn = document.querySelector('.start-game')
-const boardContainer = document.querySelector('.board-container')
+const $ = (e) => document.querySelector(e);
 
-let carList = ['bmw','audi','ford','alfa','honda','vw','fiat','mercedes','opel','volvo','bmw','audi','ford','alfa','honda','vw','fiat','mercedes','opel','volvo'];
-randomCarList = carList.sort(() => Math.random() - 0.5);
+// containers 
+const boardContainer = $('.board-container');
 
+// console.log(boardContainer);
+//btns
+// log
+const startBtn = $('.start-game');
+
+let carList = ['bmw','audi','ford','alfa','honda','vw','fiat','mercedes','opel','volvo','bmw','audi','ford','alfa','honda','vw','fiat','mercedes','opel','volvo'].sort(() => Math.random() - 0.5);
+let firstClick;
+let secoundClick;
 
 // Create card html
 const createCards = function(){
-    randomCarList.forEach(e => {
-        let cartHtml = `<div class="board">
-            <img src="img/${e}.png" alt="" class="car-img">
+    carList.forEach(car => {
+        let cartHtml = `<div class="board " id="halo">
+            <img src="img/${car}.png" alt="" class="car-img " id=${car} id="halo">
         </div>`
-        boardContainer.insertAdjacentHTML('afterbegin', cartHtml);
-
+        boardContainer.insertAdjacentHTML('afterbegin', cartHtml);    
     })
+    // setTimeout(halo(boardContainer), 5000);
+    setTimeout(() => {addHiden(boardContainer)}, 5000);
+   
+}
+const addHiden = function(container){
+    for(i = 0; i< carList.length; i++){
+        container.children[i].children[0].classList.add('hidden');
+    }
 }
 
-startBtn.addEventListener('click', createCards);
+document.addEventListener('click',function(e){ 
+    if(e.target && e.target.id == 'halo'){
+        // e.target.children[0].classList.remove('hidden');
+        // firstClick = e.target.children[0].id;
+
+    }
+});
+
+startBtn.addEventListener('click', (createCards));
