fd31fc8627410ce08b646f4f7f18bd053691545c
MatHyp <matijoker29@gmail.com>
2022-06-17 15:26:10 +0200

    added par comparison

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const $ = (e) => document.querySelector(e);
-
-// containers 
+//GetElements
 const boardContainer = $('.board-container');
 const startBtn = $('.start-game');
+const moveCounter = $('.moves-counter');
+const matchedPairs = $('.matched-pairs')
 
-let carList = ['bmw','audi','ford','alfa','honda','vw','fiat','mercedes','opel','volvo','bmw','audi','ford','alfa','honda','vw','fiat','mercedes','opel','volvo'].sort(() => Math.random() - 0.5);
+let carList = ['bmw','audi','hyundai','alfa','honda','vw','fiat','mercedes','opel','volvo','bmw','audi','hyundai','alfa','honda','vw','fiat','mercedes','opel','volvo'].sort(() => Math.random() - 0.5);
+//Game variables
 let firstClick;
 let secoundClick;
-let click = 0
-// Create card html
+let click = 0;
+let failMoves = 0;
+let matchedPair = 0;
+
+//Create cards html
 
 const createCards = function(){
     carList.forEach(car => {
@@ -17,27 +22,44 @@ const createCards = function(){
         </div>`
         boardContainer.insertAdjacentHTML('afterbegin', cartHtml);    
     })
-
-    setTimeout(() => {addHiden(boardContainer)}, 2000);
+    setTimeout(() => {hiddenForAll(boardContainer)}, 6000);
 }
 
-const addHiden = function(container){
+// Add hidden class for all carts after start
+const hiddenForAll = function(container){
     for(i = 0; i < carList.length; i++){
         container.children[i].children[0].classList.add('hidden');
     }
 }
 
-const halo1 = function(){
-    firstClick.classList.add('hidden');
-    secoundClick.classList.add('hidden');
-    firstClick = '';
-    secoundClick = '';
+// Add hidden after missed clicks
+
+const missedClicks = function(){
+    failMoves++;
+    setTimeout(() => {
+        firstClick.classList.add('hidden');
+        secoundClick.classList.add('hidden');
+        firstClick = '';
+        secoundClick = '';
+ 
+    }, 500)
+}
+//Matched cards
+const matched = function(){
+    matchedPair++;
+    matchedPairs.innerHTML = String(matchedPair)
+    if (matchedPair == 10){
+        boardContainer.preventDefault();
+    }
 }
 
-const halo = function(){
-    
+const haloo = function(){
+    if(matchedPair == 1){
+        return false
+    }
+    return true;
 }
-
+// Event for card click 
 document.addEventListener('click',function(e){ 
     if(e.target && e.target.id == 'halo'){
         e.target.children[0].classList.remove('hidden');
@@ -45,16 +67,15 @@ document.addEventListener('click',function(e){
             case 0:
                 firstClick = e.target.children[0];
                 click++;
-                console.log(firstClick);
                 break;
             case 1:
                 secoundClick = e.target.children[0];
-                let equal = (firstClick.id == secoundClick.id) ? true : halo1();
-
+                let equal = (firstClick.id == secoundClick.id) ? matched() : missedClicks();;
                 click = 0;
                 break;
         }
+        moveCounter.innerHTML = String(failMoves);
     }
-});
+}, haloo());
 
-startBtn.addEventListener('click', (createCards));
+startBtn.addEventListener('click', createCards);
